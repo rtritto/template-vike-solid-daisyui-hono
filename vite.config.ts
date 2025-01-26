3efdd54a491539af0ebedd5daeddce309e93fdb7
rtritto <ariele.1994@gmail.com>
2025-01-26 16:26:44 +0100

    Remove Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import path from 'node:path'
-import autoprefixer from 'autoprefixer'
-import tailwindcss from 'tailwindcss'
 import { plugin as vike } from 'vike/plugin'
 import { vikeNode } from 'vike-node/plugin'
 import vikeSolid from 'vike-solid/vite'
@@ -23,14 +21,6 @@ export default {
   build: {
     target: 'esnext',
     outDir: '../dist'
-  },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer
-      ]
-    }
   },
   resolve: {
     alias: {
